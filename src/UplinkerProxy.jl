8f9029c28d954b82da15b3d7bb320e21c4e3fe23
emmy <52672675+emmaccode@users.noreply.github.com>
2025-05-27 16:31:44 -0600

    changed `MAIN_PROX` to source{:backup}

--- src/UplinkerProxy.jl
+++ src/UplinkerProxy.jl
@@ -1,15 +1,29 @@
 module UplinkerProxy
 using ChiProxy
-using ChiProxy.Toolips: IP4, get_ip, get_ip4
-MAIN_PROX = proxy_route("chifidocs.com", "192.168.1.20":8000)
+using ChiProxy.Toolips: IP4, get_ip, get_ip4, style!, Connection, write!, get_route
+using ChiProxy.Toolips.Components: percent, pt
+served_cached = div("cachemsg", text = """chifidocs is currently offline; you are viewing a cached 
+    version of the site. Some functionality might be missing. Click this message to try for a live version of
+     chifidocs again.""", 
+onclick="location.href='https://chifidocs.com'", align = "center")
+
+style!(served_cached, "padding" => 2percent, "background-color" => "#1e1e1e", "color" => "white",          
+"font-weight" => "bold", "cursor" => "pointer", "position" => "fixed", "top" => 95percent, "height" => 3percent, 
+"width" => 96percent, "left" => 0percent)
+
+MAIN_PROX = ChiProxy.backup_proxy_route("chifidocs.com", "192.168.1.28":8000, served_cached) do c::Connection
+    write!(c, "unable to reach chifidocs, and no cache of $(get_route(c)) available")
+end
+
 MEMCODE::String = ""
 
-function start(ip::IP4 = "127.0.0.1":8000, uplinker_ip::IP4 = "127.0.0.1":8001)
+function start(ip::IP4 = "127.0.0.1":8000, uplinker_ip::IP4 = "127.0.0.1":8001; noup::Bool = false)
     ChiProxy.start(ip, MAIN_PROX)
+    if noup return end
     ChiProxy.Toolips.start!(:TCP, UplinkerProxy, uplinker_ip)
 end
 
-uplinker_handler = ChiProxy.Toolips.TCPHandler() do c::ChiProxy.Toolips.SocketConnection
+uplinker_handler = ChiProxy.Toolips.Handler() do c::ChiProxy.Toolips.SocketConnection
     sent_bytes = String(readavailable(c))
     rq_type = ["S", "I"][parse(Int64, sent_bytes[1])]
     if rq_type == "S"
@@ -28,7 +42,7 @@ uplinker_handler = ChiProxy.Toolips.TCPHandler() do c::ChiProxy.Toolips.SocketCo
         @info "got I rqtype"
         code_set = split(sent_bytes[2:end], "|")
         if code_set[1] == UplinkerProxy.MEMCODE
-            MAIN_PROX.ip = IP4(get_ip(c), parse(UInt16, code_set[2]))
+            MAIN_PROX.src[:to] = IP4(get_ip(c), parse(UInt16, code_set[2]))
             @info "set ip $(get_ip(c))"
         else
             @warn UplinkerProxy.MEMCODE
